9e9b9bf5d75e1b93be83c08ceb620c01d19486bd
Gustavonvp <gustavonvp@gmail.com>
2022-01-16 05:48:16 -0300

    using interface and set timeout - setinterval - pipe process

--- src/app/notifications/notifications.service.ts
+++ src/app/notifications/notifications.service.ts
@@ -1,9 +1,75 @@
 import { Injectable } from '@angular/core';
+import { Observable, pipe, Subject } from 'rxjs';
+import { scan } from 'rxjs/operators';
+
+interface Command {
+  id: number;
+  type: 'success' | 'error' | 'clear';
+  text?: string;
+}
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class NotificationsService {
 
-  constructor() { }
+  messagesInput: Subject<Command>;
+  messagesOutput: Observable<Command []>;
+
+  constructor() { 
+    this.messagesInput = new Subject<Command>()
+    this.messagesOutput = this.messagesInput.pipe(
+      scan((acc: Command[], value: Command) => {
+        if(value.type === 'clear') {
+          return acc.filter(message => message.id !== value.id);
+        }else {
+          return [...acc, value];
+        }
+       }, [])
+    )
+
+  }
+
+
+  getMessages() {
+    return this.messagesInput.pipe(
+      scan((acc: Command[],value: Command) => {
+        if(value.type ==='clear') {
+          return acc.filter(message => message.id !== value.id);
+        } else {
+          return [...acc, value];
+        }
+      }, [])
+    );
+  }
+
+  addSuccess(message: string) {
+      this.messagesInput.next({
+        id: this.randomId(),
+        text: message,
+        type: 'success'
+      });
+  }
+
+  addError(message: string){
+    this.messagesInput.next({
+      id: this.randomId(),
+      text: message,
+      type: 'error'
+    });
+    
+  }
+
+  clearMessage(id: number){
+    this.messagesInput.next({
+      id,
+      type: 'clear',
+
+    });
+  }
+
+  private randomId() {
+      return Math.round(Math.random() * 10000);
+  }
 }
